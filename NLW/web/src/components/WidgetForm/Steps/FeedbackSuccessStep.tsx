250ae23aace51f1087586b26c4b5a64fa26f3fe8
phr31 <48303421+phr31@users.noreply.github.com>
2022-06-02 22:29:57 -0300

    front-end concluded

--- NLW/web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ NLW/web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,5 +1,29 @@
-export function FeedbackSuccessStep(){
-  return(
-    <div></div>
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackSuccessStepProps{
+  onFeedbackRestartRequested: () => void
+}
+
+export function FeedbackSuccessStep({onFeedbackRestartRequested}: FeedbackSuccessStepProps) {
+  return (
+    <>
+      <header>
+        <CloseButton />
+      </header>
+      <div className="flex flex-col items-center py-10 w-[384px]">
+
+        <div className="flex flex-col items-center py-0">
+          <img src="https://i.gifer.com/3cJv.gif" className=' w-full h-full rounded-lg'/>
+        </div>
+        <span className=" text-xl mt-2 justify-center">Agradecemos o seu Feedback!</span>
+        <button 
+        type="button"
+        onClick={onFeedbackRestartRequested}
+        className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50">
+          Quero enviar um novo Feedback...
+        </button>
+      </div>
+    </>
   )
 }
+
--- NLW/web/src/components/WidgetForm/index.tsx
+++ NLW/web/src/components/WidgetForm/index.tsx
@@ -5,6 +5,7 @@ import ideaImageURL from '../../assets/idea.png'
 import thoughtImageURL from '../../assets/thought.png'
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 
 export const feedbackTypes = {
@@ -35,27 +36,34 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 //Formato para a aplicação
 export function WidgetForm() {
-
   //feedback do usuario a ser armazenado caso o mesmo insira um, ou permanece nulo
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  //feedback enviado
+  const [feedbackSend, setFeedbackSend] = useState(false)
 
   //retornar a tela anterior
   function handleRestartFeedback() {
+    setFeedbackSend(false)
     setFeedbackType(null)
   }
 
   return (
     <div
       className="bg-zinc-900 p-4 relative rounded-2xl  ab-4 flex flex-col items-center shadow-lg  w-[calc(100vw-2rem)] md:w-auto">
-
-
-      {!feedbackType ? ( //fornecer acesso a função onFeedbackTypeChange fornecendo seu componente como propriedade
-        <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+      {feedbackSend ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
       ) : (
-        <FeedbackContentStep
-          feedbackType={feedbackType}
-          onFeedbackRestartRequested={handleRestartFeedback}
-        />
+        <>
+          {!feedbackType ? ( //fornecer acesso a função "feedback" correspondente, fornecendo seu componente como propriedade
+            <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSend(true)}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-xs text-neutral-400">
